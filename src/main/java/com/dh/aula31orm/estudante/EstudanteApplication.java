5ce546a1eb54fc0a6170559b7a06d31db99dd47b
Mateus Benites Dias <imattdias@gmail.com>
2022-03-31 10:22:32 -0300

    CTD - Back End I - Aula 31 - Exercício Sync Estudante

---
+++ src/main/java/com/dh/aula31orm/estudante/EstudanteApplication.java
@@ -0,0 +1,13 @@
+package com.dh.aula31orm.estudante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/aula31orm/estudante/EstudanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.aula31orm.estudante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
